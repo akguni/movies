933f9d70ef50f4ae87a879f6573cd9c8133f02b8
Ismail Akgun <68012780+akguni@users.noreply.github.com>
2020-11-08 09:15:19 +0100

    Update application.py

--- application.py
+++ application.py
@@ -31,6 +31,7 @@ app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+app.secret_key = "dev"
 
 # Configure CS50 Library to use SQLite database
 db = SQL("postgres://***REMOVED***")
