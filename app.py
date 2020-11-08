701b46f651b75c8eea0a40cf4899312742d9ba65
Ismail <akguni@yahoo.com>
2020-11-08 19:14:48 +0100

    commented out local sessions lines

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import os
 
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
-from flask_session import Session
-from tempfile import mkdtemp
+#from flask_session import Session
+#from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -27,10 +27,12 @@ def after_request(response):
 app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+#app.config["SESSION_FILE_DIR"] = mkdtemp()
+#app.config["SESSION_PERMANENT"] = False
+#app.config["SESSION_TYPE"] = "filesystem"
+#Session(app)
+
+app.secret_key ='***REMOVED***'
 
 # Configure CS50 Library to use SQLite database
 db = SQL("postgres://***REMOVED***")
